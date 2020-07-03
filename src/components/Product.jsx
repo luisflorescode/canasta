6ada6ec4b6332069ffd90d265972ac2f82219a46
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-07-03 01:58:59 -0500

    Product component added

---
+++ src/components/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../assets/styles/components/Product.scss';
+
+const Product = ({ photo, name, store, price }) => (
+  <li className="product">
+    <img className="product__photo" src={photo} alt={name} />
+    <div className="product__details">
+      <h3 className="product__details__name">{name}</h3>
+      <span className="product__details__store">{store}</span>
+      <strong className="product__details__price">{price}</strong>
+    </div>
+  </li>
+);
+
+Product.propTypes = {
+  photo: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  store: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+};
+
+export default Product;
